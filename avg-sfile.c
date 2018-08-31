4edfb808b7a1ef06bf2ab0b514e175eb61256cf8
Christopher Batten <cbatten@cornell.edu>
2018-08-30 22:38:11 -0400

    Create avg-sfile.c

---
+++ avg-sfile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int avg( int x, int y )
+{
+  // insert code to calculate average here
+}
+
+int main()
+{
+  int a = 10;
+  int b = 20;
+  int c = avg( a, b );
+  printf( "average of %d and %d is %d\n", a, b, c );
+  return  0;
+}
